fa67d744558797f261f8b713b2038e3c3a478f6f
Mathias Rav <rav@cs.au.dk>
2017-05-28 13:13:01 -0400

    Fix crop.py

--- crop.py
+++ crop.py
@@ -3,14 +3,17 @@ import re
 import argparse
 import itertools
 import collections
-from raster import iterrows, raster_sink, peek_dtype
+from raster import (
+    iterrows, write_raster_base, peek_dtype,
+    dummy_progress, show_progress, iterprogress,
+)
 
 
 Spec = collections.namedtuple('Spec', 'width height left top')
 
 
 def parse_spec(s):
-    mo = re.parse(r'^(\d+)x(\d+)\+(\d+)\+(\d+)$', s)
+    mo = re.match(r'^(\d+)x(\d+)\+(\d+)\+(\d+)$', s)
     if not mo:
         raise ValueError('format: WIDTHxHEIGHT+LEFT+TOP')
     return Spec(*map(int, mo.groups()))
@@ -23,15 +26,31 @@ def main():
     parser.add_argument('-s', '--spec', required=True,
                         help='WIDTHxHEIGHT+LEFT+TOP', type=parse_spec)
     args = parser.parse_args()
+    width, height, left, top = args.spec
 
-    meta, rows = iterrows(args.input, meta=True)
+    meta, rows = iterrows(args.input, meta=True, pi=dummy_progress)
+
+    input_geo = meta.GetGeoTransform()
+    origin_x, pixel_width, zero, origin_y, zero_, pixel_height = input_geo
+    assert zero == zero_ == 0
+    output_geo = [origin_x + left * pixel_width, pixel_width, 0,
+                  origin_y + top * pixel_height, 0, pixel_height]
+
+    def handle_ds(ds):
+        ds.SetGeoTransform(output_geo)
+        ds.SetProjection(meta.GetProjection())
+
     dtype, rows = peek_dtype(rows)
-    rows = itertools.islice(rows, args.spec.top,
-                            args.spec.top + args.spec.height)
-    i = args.spec.left
-    j = args.spec.left + args.spec.width
-    rows = (row[i:j] for row in rows)
-    raster_sink(args.output, rows, dtype, meta)
+    rows = itertools.islice(rows, top, top + height)
+    rows = (row[left:left+width] for row in rows)
+    rows = iterprogress(rows, show_progress(args.output), height)
+
+    def handle_band(band):
+        for i, row in enumerate(rows):
+            band.WriteArray(row.reshape(1, -1), 0, i)
+
+    write_raster_base(args.output, dtype, handle_band, handle_ds,
+                      width, height)
 
 
 if __name__ == '__main__':
