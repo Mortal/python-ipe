fc771e0b8ecf4548111416afa872cd1537ab43be
Mathias Rav <m@git.strova.dk>
2018-07-05 09:25:23 +0200

    Add adjust_origin.py

---
+++ adjust_origin.py
@@ -0,0 +1,41 @@
+"""
+Given an Ipe selection of a path and a text, where both have
+transformations="translations", move the origin of the path to the text.
+This essentially groups the two objects together
+when the entire drawing is scaled.
+
+Only tested for polylines, not for arcs/curves.
+"""
+
+import re
+import sys
+from xml.etree import ElementTree as ET
+
+
+def get_origin(o):
+    x, y = map(float, (o.get("pos") or "0 0").split())
+    a, b, c, d, e, f = map(float, (o.get("matrix") or "1 0 0 1 0 0").split())
+    assert o.get("transformations") == "translations"
+    return a * x + c * y + e, b * x + d * y + f
+
+
+def main():
+    s = sys.stdin.read()
+    tree = ET.fromstring(s)
+    assert tree.tag == "ipeselection"
+    path, text = sorted(tree, key=lambda n: n.tag)
+    assert path.tag == "path"
+    assert text.tag == "text"
+    px, py = get_origin(path)
+    tx, ty = get_origin(text)
+    path.set("matrix", "1 0 0 1 %g %g" % (tx, ty))
+
+    def translate(mo):
+        return "%g %g" % (float(mo.group(1)) + px - tx, float(mo.group(2)) + py - ty)
+
+    path.text = re.sub(r"(-?[0-9.]+) (-?[0-9.]+)", translate, path.text)
+    print(ET.tostring(tree, encoding="unicode", method="xml"))
+
+
+if __name__ == "__main__":
+    main()
